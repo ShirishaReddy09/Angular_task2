3e7ea679fe1fd81c0240d7a903c035e3f2a75743
ShirishaReddy09 <pathishirisha9@gmail.com>
2021-10-04 13:50:41 +0530

    Initial commit

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { task } from '../model';
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  taskvalue="";
+  currDay=(new Date()).getDay();
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  taskArray:Array<task> = [
+    {
+      title:"default task",
+      status:true
+    }
+  ];
+  addTask(){
+    
+
+    let taskobject={
+      title:this.taskvalue,
+      status:true
+    }
+    this.taskArray.push(taskobject);
+    this.taskvalue='';
+
+  }
+  deleteTask(index:number){
+    this.taskArray.splice(index,1);
+  }
+
+  completeTask(index:number){
+    if(this.taskArray[index].status){
+      this.taskArray[index].status=false;
+    }
+    else{
+      this.taskArray[index].status=true;
+    }
+    
+  }
+
+  removeAll(){
+    console.log(this.taskArray);
+    let completeArr:number[]=[];
+    this.taskArray.forEach((task,index)=>{
+      if(task.status==false)
+      {
+        completeArr.push(index);
+      }
+    });
+    if(completeArr.length==0)
+    {
+      alert("Please select the tasks");
+    }
+    else{
+      for(var i=completeArr.length-1;i>=0;i--)
+      {
+        this.taskArray.splice(completeArr[i],1);
+      }
+    }
+  }
+  totalTask(){
+    return this.taskArray.length;
+  }
+}
